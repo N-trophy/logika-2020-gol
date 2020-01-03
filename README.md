e38964ca661d61f6dcdc3569c8dc0a050c2d97f7
Jan Horacek <me@apophis.cz>
2020-01-03 13:00:33 +0100

    Add web settings.

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # N-trophy 2020 qualification task web
 
 Game of Life
+
+## How to run server for development
+
+```
+$ cp gol/settings/development.py.txt gol/settings/development.py
+Edit gol/settings/development.py as you wish.
+$ virtualenv -p python3 djangoenv
+$ source djangoenv/bin/activate
+$ pip3 install -r requirements.txt
+$ ./manage.py collectstatic --settings=gol.settings.development
+$ ./manage.py runserver --settings=gol.settings.development
+```
+
+## How to deploy to production
+
+```
+$ cp gol/settings/production.py.txt gol/settings/production.py
+Edit gol/settings/development.py as you wish.
+$ ./manage.py collectstatic --settings=gol.settings.production
+```
+
+## How to create db structure
+
+```
+$ ./manage.py migrate --settings=gol.settings.development
+$ ./manage.py makemigrations gol --settings=gol.settings.development
+$ ./manage.py migrate gol --settings=gol.settings.development
+```
--- gol/settings/base.py
+++ gol/settings/base.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'o6u07k79kzfy^(5l!da%snq$0g9&4=kx-i(m=1t)7q-s627+2&'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -69,18 +56,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'gol.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
@@ -103,14 +78,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
+LANGUAGE_CODE = 'cs'
+TIME_ZONE = 'Europe/Prague'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
@@ -118,3 +89,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+EMAIL_PORT = 465
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.onebit.cz'
+EMAIL_HOST_USER = 'logika-send@ntrophy.cz'
+EMAIL_USE_SSL = True
