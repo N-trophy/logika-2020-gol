636b914672d2dc8f68946555355e217db17965b5
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2020-01-17 15:55:56 +0100

    Add login and logout pages.

--- gol/urls.py
+++ gol/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 from django.urls import path
 
 import gol.rules_parser_endpoint as rules_parser
@@ -22,6 +24,9 @@ import gol.views as views
 urlpatterns = [
     path('', views.index),
     path('admin/', admin.site.urls),
+    path('login', auth_views.LoginView.as_view(), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('accounts/profile/', lambda request: redirect('/', permanent=False)),
     path('rules/parse', rules_parser.parse_rules),
     path('automata', views.automata),
 ]
--- gol/views.py
+++ gol/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 
 
+@login_required(login_url='/login')
 def index(request, *args, **kwargs):
     return render(request, "index.html")
 
 
-@login_required(login_url='/admin')
+@login_required(login_url='/login')
 def automata(request):
     return render(request, "automata.html")
