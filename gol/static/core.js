57b81a9473bcdabd4d577378a3bda0bb4e832337
Ondřej Svoboda <xsvobo25@fi.muni.cz>
2020-01-19 12:39:25 +0100

    Separate navbar

--- gol/static/core.js
+++ gol/static/core.js
@@ -1,8 +1,8 @@
 function toggleNav() {
-    var x = document.getElementById("navDemo");
+    var x = document.getElementById("navSmall");
     if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
+        x.className += " w3-show";
     } else {
-      x.className = x.className.replace(" w3-show", "");
+        x.className = x.className.replace(" w3-show", "");
     }
 }
